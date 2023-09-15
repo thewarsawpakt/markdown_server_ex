94e3fdece8c1ff1644cb8437744a5044a751322c
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-14 20:48:25 -0500

    [cache] added type hints and finalized procedures

--- lib/markdown_cache.ex
+++ lib/markdown_cache.ex
@@ -1,26 +1,40 @@
 defmodule MarkdownCache do
   @moduledoc """
-  "One could say caching for a blog server is akin to putting lipstick on a pig, yet there is beauty in contradiction"
+  'One could say using a cache for a blog server is akin to putting lipstick on a pig, yet there is beauty in contradiction'
 
-  GenServer-powered cache server. uses an Agent for storing state
+  GenServer-powered cache server. Uses an Agent for storing state.
   """
   use GenServer
 
   @posts_dir "posts"
 
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  @spec init(any) :: {:error, any} | {:ok, pid}
   def init(_) do
-    {:ok, }
+    MarkdownCacheAgent.start_link()
   end
 
-  def handle_call({:read, filename}, _from, state) do
-    # If the file is not found, bubble up the exception such that the caller can return a 404
-    # TODO: use Registry to communicate with cache agent
-    {:ok, binary} = File.read("#{@posts_dir}/#{filename}")
-    if !state.has_key?(filename) do
-      state.put(%{last_access_time: DateTime.utc_now(), filename: filename, contents: binary})
+  @spec handle_call({:read, String}) :: {:reply, :enoent | binary}
+  def handle_call({:read, filename}) do
+    cache = Agent.get(MarkdownCacheAgent, fn cache -> cache end)
+    case File.read("#{@posts_dir}/#{filename}") do
+      {:ok, contents} ->
+        if !cache.has_key?(filename) do
+          Agent.update(MarkdownCacheAgent, fn cache -> Map.put(cache, filename, %CacheEntry{last_access_time: DateTime.utc_now(), contents: contents}) end)
+        end
+        {:reply, contents}
+      {:error, :enoent} ->
+        {:reply, :enoent}
+      # TODO: handle other possible errors
     end
-    {:reply, binary}
   end
 
+  @spec read(String) :: {:reply, :enoent | binary}
+  def read(filename) do
+    GenServer.call(__MODULE__, MarkdownCache.handle_call({:read, filename}))
+  end
 
 end
