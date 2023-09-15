e8cb6f28f7654ea8da5a435eb507638e80c1e198
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-14 20:57:36 -0500

    [cache] added type hints for cache agent

--- lib/markdown_cache_agent.ex
+++ lib/markdown_cache_agent.ex
@@ -4,16 +4,19 @@ defmodule MarkdownCacheAgent do
   """
   use Agent
 
+  @spec start_link :: {:error, any} | {:ok, pid}
   def start_link() do
     Agent.start_link(fn -> Map.new() end, name: __MODULE__)
   end
 
+  @spec cache({Date, String, binary}) :: :ok
   def cache({last_access_time, filename, contents}) do
     Agent.update(__MODULE__, fn state ->
       Map.put(state, filename, %{last_access_time: last_access_time, contents: contents})
     end)
   end
 
+  @spec purge :: nil
   def purge() do
   end
 end
