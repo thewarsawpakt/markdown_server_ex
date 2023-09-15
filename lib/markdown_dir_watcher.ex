bc91af9127037488e63445308f47bd8cca06ede5
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-15 07:25:17 -0500

    [watcher] updated ambiguous comment

--- lib/markdown_dir_watcher.ex
+++ lib/markdown_dir_watcher.ex
@@ -2,7 +2,7 @@ defmodule MarkdownDirWatcher do
   # transient means the task will always be restarted
   use Task, restart: :transient
 
-  # 60 seconds
+  # 60 seconds in ms
   @interval 60 * 1_000
 
   def start_link(arg) do
