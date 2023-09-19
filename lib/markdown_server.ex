14e2399e2024ae8d989f009a3ee405e70f460811
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-19 08:21:28 -0500

    [web] added web server component

--- lib/markdown_server.ex
+++ lib/markdown_server.ex
@@ -1,14 +1,25 @@
-defmodule MarkdownServer do
-  @moduledoc """
+defmodule MarkdownServer.Supervisor do
+  @moduledoc"""
   Runs a server that serves Markdown files.
   """
 
-  def init(_) do
+  @port 5000 # todo: move this to an env variable
+  def start(_, _) do
     children = [
-      MarkdownCache,
-      MarkdownCacheAgent
+      %{
+        id: MarkdownCache,
+        start: {MarkdownCache, :start_link, []},
+      },
+      %{
+        id: MarkdownCacheAgent,
+        start: {MarkdownCacheAgent, :start_link, []}
+      },
+      %{
+        id: MarkdownWebServer,
+        start: {MarkdownWebServer, :accept, [@port]},
+      },
     ]
 
-    {:ok, _} = Supervisor.init(children, strategy: :one_for_one)
+    {:ok, _} = Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
