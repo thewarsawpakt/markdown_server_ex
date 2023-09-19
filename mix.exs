395e77cf50e69c96f3f79c2db635784cdd3a3141
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-19 08:20:27 -0500

    [app] fixed initialization code

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule MarkdownServer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {MarkdownServer.Supervisor, []},
       extra_applications: [:logger]
     ]
   end
