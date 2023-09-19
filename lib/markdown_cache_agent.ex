faa0d3ccc4e492efadd1f8d01c1396ef02b6d9fe
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-09-19 08:20:39 -0500

    [cache] added cache max entries

--- lib/markdown_cache_agent.ex
+++ lib/markdown_cache_agent.ex
@@ -1,7 +1,9 @@
 defmodule MarkdownCacheAgent do
+  require Logger
   @doc """
     We use this agent to preserve the cache state in the event of a crash.
   """
+  @max_cache_entries 12 # This is a rather arbitrary limit, but it can be changed later.
   use Agent
 
   @spec start_link :: {:error, any} | {:ok, pid}
@@ -13,6 +15,21 @@ defmodule MarkdownCacheAgent do
   def cache({last_access_time, filename, contents}) do
     Agent.update(__MODULE__, fn state ->
       Map.put(state, filename, %{last_access_time: last_access_time, contents: contents})
+    end)
+  end
+
+
+  @doc """
+    Adds a thin layer above the internal Map
+  """
+  @spec has_key(atom()) :: :ok
+  def has_key(filename) do
+    Logger.debug("request has_key for filename #{filename}")
+    Agent.get(__MODULE__, fn state ->
+      case Map.fetch(state, String.to_atom(filename)) do
+        {:ok, _} -> true
+        :error -> false
+      end
     end)
   end
 
